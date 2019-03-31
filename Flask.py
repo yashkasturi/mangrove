eeafe14c72beb57acaa132d106144a89655b7bd9
yashkasturi <37289085+yashkasturi@users.noreply.github.com>
2019-03-31 17:05:08 +0530

    Update Flask.py

--- Flask.py
+++ Flask.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def css():
-    return render_template('mangrove.html')
+    return "Mangrove"
 	
 @app.route('/location.html/')
 def about():
@@ -16,8 +16,7 @@ def about():
 def detect():
     return final.main
 	
-if __name__ == '__main__':
-	app.run(port = 3000)
+
 	
 
 	
