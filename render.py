ccfc952ad7b0a04ee73f6c40d6b236977f71429c
yashkasturi <37289085+yashkasturi@users.noreply.github.com>
2019-03-31 17:03:11 +0530

    Delete render.py

--- render.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-
-with open("mangrove_Index.html") as fp:
-    soup = BeautifulSoup(fp)
-
-soup = BeautifulSoup("<html>data</html>")
-
-BeautifulSoup("Sacr&eacute; bleu!")
